d885cd494d64214fb5acb6a0dc6e78b861145b69
Abdulrahmen Almodaimegh <abdulal@bu.edu>
2022-01-14 03:22:41 +0300

    Added IMGUI_API define to premake

--- premake5.lua
+++ premake5.lua
@@ -22,7 +22,8 @@ project "ImGui"
 
     defines
     {
-        IMGUI_IMPL_OPENGL_LOADER_CUSTOM 
+        IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+        IMGUI_API = __declspec(dllexport)       -- Windows specific!!!
     }
 
 	filter "system:windows"
